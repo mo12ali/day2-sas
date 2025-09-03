f33e65fdeb9c7385cdd766a707f7efb5f4fa7b9f
mo12ali <mohammedalicherraoui@gmail.com>
2025-09-03 11:13:28 +0100

    exo de supermarche

---
+++ exosup.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct{
+   int nb; 
+   double prix;
+}produits;
+int main(){
+   produits stock[3] = {
+        {1, 125.00},
+        {2, 2.0},
+        {3, 2.5}
+    };
+    int n;
+    double total = 0.00 , prct = 0.00;
+    int id;
+
+    printf("id et nombre de produit : ");
+
+    scanf("%d %d",&id,&n);
+
+   
+
+
+    for(int i = 0; i<sizeof(stock)/sizeof(produits); i++){
+        if(stock[i].nb == id){
+            for(int j = 1 ; j <= n ; j++){
+                total += (stock[i].prix - stock[i].prix * prct);
+                prct += 0.02;
+            }
+        }
+    }
+    /**
+     * 
+     * 
+     * t1 = 125 + (125 - 125*0%)
+     * t2 = t1 + (125 - 125*2%)
+     */
+
+    printf("le prix a payer est : %.2f",total);
+
+
+    return 0;
+}
---
+++ mini-projet.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct{
+    char titre[20];
+    char auteur[20];
+    double prix;
+    int qte;
+}livre;
+
+typedef struct{
+    livre st;
+    int capacite;
+}stock;
+
+//1 ajouter livre
+void ajouter_livre(livre *stock, livre aj){
+
+    
+}
+
+//2 afficher tous les livres disponibles
+void afficher_tous_les_livres(){
+
+}
+//3 mettre a bour la quantité d'un livre
+//4 supprimer un livre du stock 
+//5 afficher le nombre total de livres en stock 
+
+int main(){
+
+
+    return 0;
+}
